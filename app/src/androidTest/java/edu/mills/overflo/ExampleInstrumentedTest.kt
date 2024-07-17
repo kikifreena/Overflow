1a96ad4a3914ee4f4cc282c13069129f03d4573b
kikifreena <14kobarbara@gmail.com>
2024-07-16 17:30:42 -0700

    add compose + detekt tooling

--- app/src/androidTest/java/edu/mills/overflo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/edu/mills/overflo/ExampleInstrumentedTest.kt
@@ -1,26 +1,24 @@
-package edu.mills.overflo;
+package edu.mills.overflo
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see [Testing documentation](http://d.android.com/tools/testing)
  */
-@RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
     @Test
-    public void useAppContext() {
+    fun useAppContext() {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+        val appContext = ApplicationProvider.getApplicationContext<Context>()
 
-        assertEquals("edu.mills.overflo", appContext.getPackageName());
+        Assert.assertEquals("edu.mills.overflo", appContext.packageName)
     }
 }
--- app/src/main/java/edu/mills/overflo/MainActivity.kt
+++ app/src/main/java/edu/mills/overflo/MainActivity.kt
@@ -1,67 +1,66 @@
-package edu.mills.overflo;
+package edu.mills.overflo
 
-import android.graphics.Color;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.GridLayout;
-import android.view.View;
-import android.widget.TextView;
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.GridLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-public class MainActivity extends AppCompatActivity {
-    private static final int sideSize = 4;
-    private static int size;
-    private static TextView[] textViews;
-    private static Grid g;
-    private static int next;
+class MainActivity : AppCompatActivity() {
+    private lateinit var textViews: Array<TextView?>
+    private var size = 0
+    private var g: Grid? = null
+    private var next = 0
 
-    private void drawGrid() {
-        for (final Square s : g.getSquares()) {
-            int i = s.getId();
-            textViews[i].setText(Integer.toString(s.getValue()));
-            if (s.getPlayer() == 1) {
-                textViews[i].setTextColor(getResources().getColor(R.color.blue));
+    private fun drawGrid() {
+        for (s in g!!.squares) {
+            val i = s.id
+            textViews[i]!!.text = s.value.toString()
+            if (s.player == 1) {
+                textViews[i]!!.setTextColor(resources.getColor(R.color.blue, theme))
             }
-            if (s.getPlayer() == 2) {
-                textViews[i].setTextColor(getResources().getColor(R.color.gold));
+            if (s.player == 2) {
+                textViews[i]!!.setTextColor(resources.getColor(R.color.gold, theme))
             }
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        GridLayout gridLayout = findViewById(R.id.grid);
-        size = sideSize * sideSize;
-        gridLayout.setColumnCount(sideSize);
-        textViews = new TextView[size];
-        g = new Grid(sideSize);
-        for (final Square s : g.getSquares()) {
-            int i = s.getId();
-            textViews[i] = new TextView(this);
-            textViews[i].setText(Integer.toString(s.getValue()));
-            textViews[i].setTextSize(80);
-            textViews[i].setPadding(5, 5, 5, 5);
-            textViews[i].setId(i);
-            textViews[i].setTextColor(Color.GRAY);
-            textViews[i].setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    next = g.oneTurn(s);
-                    View colorView = findViewById(R.id.turn_color);
-                    TextView textView = findViewById(R.id.turn_text);
-                    if (next == 1) {
-                        colorView.setBackgroundColor(getResources().getColor(R.color.blue));
-                        textView.setText(R.string.player_1_turn);
-                    }
-                    if (next == 2) {
-                        colorView.setBackgroundColor(getResources().getColor(R.color.gold));
-                        textView.setText(R.string.player_2_turn);
-                    }
-                    drawGrid();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val gridLayout = findViewById<GridLayout>(R.id.grid)
+        size = sideSize * sideSize
+        gridLayout.columnCount = sideSize
+        textViews = arrayOfNulls(size)
+        g = Grid(sideSize)
+        for (s in g!!.squares) {
+            val i = s.id
+            textViews[i] = TextView(this)
+            textViews[i]!!.text = s.value.toString()
+            textViews[i]!!.textSize = 80f
+            textViews[i]!!.setPadding(5, 5, 5, 5)
+            textViews[i]!!.id = i
+            textViews[i]!!.setTextColor(Color.GRAY)
+            textViews[i]!!.setOnClickListener {
+                next = g!!.oneTurn(s)
+                val colorView = findViewById<View>(R.id.turn_color)
+                val textView = findViewById<TextView>(R.id.turn_text)
+                if (next == 1) {
+                    colorView.setBackgroundColor(resources.getColor(R.color.blue))
+                    textView.setText(R.string.player_1_turn)
                 }
-            });
-            gridLayout.addView(textViews[i]);
+                if (next == 2) {
+                    colorView.setBackgroundColor(resources.getColor(R.color.gold))
+                    textView.setText(R.string.player_2_turn)
+                }
+                drawGrid()
+            }
+            gridLayout.addView(textViews[i])
         }
+    }
+
+    companion object {
+        private const val sideSize = 4
     }
 }
